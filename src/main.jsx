7848492aa03e29f9eb2d0aac66104e59e89c2780
asshikhon <asshikhon@gmail.com>
2024-04-26 04:34:56 +0600

    install firebse and hide firebaseConfig

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes/Routes.jsx'
+import AuthProviders from './providers/AuthProviders.jsx'
+import { HelmetProvider } from 'react-helmet-async'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+      <AuthProviders>
+      <HelmetProvider>
       <RouterProvider router={router} />
+    </HelmetProvider>
+      </AuthProviders>
   </React.StrictMode>,
 )
